f03e7fe4fb6cafbd9698576ff4bf464cc7acf893
Vanilton Filho <vanilton.alv3sfilho@gmail.com>
2023-08-13 09:55:06 -0300

    feat: initial commit, learn about arrays

---
+++ arrays/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x [10]int
+
+	fmt.Println(x)
+	fmt.Println(len(x))
+
+	x[0] = 2
+	x[1] = 6
+	x[2] = 12
+	fmt.Println(x)
+	fmt.Println(x[0])
+
+	var y [10]string
+	fmt.Println(y)
+	y[0] = "golang"
+	fmt.Println(y)
+
+	z := [5]int{5, 10, 4, 5, 6}
+	fmt.Println(z)
+
+}
