5785330076b0ae1692f5bc841887418a7d2bc5ef
Vanilton Filho <vanilton.alv3sfilho@gmail.com>
2023-08-21 22:32:10 -0300

    feat: learn about inheritance

--- structs/main.go
+++ structs/main.go
@@ -8,16 +8,26 @@ type Car struct {
 	Color string
 }
 
+type SuperCar struct {
+	Car
+	CanFly bool
+	Name   string
+}
+
 func (c Car) info() string {
 	return fmt.Sprintf("Car: %s\nYear: %d\nColor: %s\n", c.Name, c.Year, c.Color)
 }
 
 func main() {
 
-	car := Car{"Corolla", 2017, "White"}
-	car2 := Car{"BMW 320i", 2017, "Black"}
+	// car := Car{"Corolla", 2017, "White"}
+	// car2 := Car{"BMW 320i", 2017, "Black"}
+	sCar := SuperCar{Car: Car{"Fusca", 2030, "Blue"}, CanFly: true, Name: "Elantra"}
 
-	fmt.Println(car.Name, car.Year, car.Color)
-	fmt.Println(car2.Name, car2.Year, car.Color)
-	fmt.Println(car.info())
+	// fmt.Println(car.Name, car.Year, car.Color)
+	// fmt.Println(car2.Name, car2.Year, car.Color)
+	// fmt.Println(car.info())
+
+	fmt.Println(sCar.Name)
+	fmt.Println(sCar.info())
 }
